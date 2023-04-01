6aeda4a7aa39ae6f8b6a75675351ff6512bc8bc3
MedelyPharma <bharattiwary@bhawnas-air.lan>
2023-04-01 14:20:10 -0700

    Update deployfe.sh

--- deployfe.sh
+++ deployfe.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "Workspace: $1"
 cd frontend
-echo "Installing angular dependencies"
+echo "Installing angular dependencies . . ."
 npm ci
-echo "Building angular project, with output path $1/frontend-server/dist/reiphy-pharma"
+echo "Building angular project, with output path $1/frontend-server/dist/reiphy-pharma  . . ."
 npm run build-production -- --output-path $1/frontend-server/dist/reiphy-pharma
 cd ../frontend-server
-echo "Installing frontend server dependencies"
+echo "Installing frontend server dependencies  . . ."
 npm ci
-echo "Building frontend server"
+echo "Building frontend server  . . ."
 npm run build --if-present
